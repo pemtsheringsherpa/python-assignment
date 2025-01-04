b4fdb86d6f1db66c03acc275edf1bf1f03e07e65
pem tshering sherpa <pem.20251061@nami.edu.np>
2025-01-04 20:44:03 +0545

    Add files via upload

---
+++ Ask the user to enter their age.py
@@ -0,0 +1,8 @@
+# Ask the user to enter their age
+age = int(input("Enter your age: "))
+
+# Check if the user is eligible to vote
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
